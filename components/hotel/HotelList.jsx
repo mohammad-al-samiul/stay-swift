006ff05a1ef60836545afd797f882a9434359827
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-07-31 16:12:46 +0600

    getAllHotels function added

--- components/hotel/HotelList.jsx
+++ components/hotel/HotelList.jsx
@@ -1,10 +1,13 @@
+import { getAllHotels } from "@/database/queries/hotels";
 import HotelCard from "./HotelCard";
 
-const HotelList = () => {
+const HotelList = async () => {
+  const allHotels = await getAllHotels();
+
   return (
     <div className="col-span-9">
       <div className="space-y-4">
-       <HotelCard />
+        <HotelCard />
       </div>
     </div>
   );
--- database/queries/hotels/index.js
+++ database/queries/hotels/index.js
@@ -1,5 +1,7 @@
 import { HotelModel } from "@/models/hotel.model";
+import { replaceMongoIdInArray } from "@/utils/data-utils";
 
 export async function getAllHotels() {
   const hotels = await HotelModel.find().lean();
+  return replaceMongoIdInArray(hotels);
 }
