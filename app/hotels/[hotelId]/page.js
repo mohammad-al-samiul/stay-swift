c884962c41f7c00201eadd293696a09cf98d13cd
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-07-31 12:33:59 +0600

    hotel details added

---
+++ app/hotels/[hotelId]/page.js
@@ -0,0 +1,15 @@
+import Gallery from "@/components/hotel/details/Gallery";
+import Overview from "@/components/hotel/details/Overview";
+import Summary from "@/components/hotel/details/Summary";
+
+const HotelDetails = () => {
+  return (
+    <>
+      <Summary />
+      <Gallery />
+      <Overview />
+    </>
+  );
+};
+
+export default HotelDetails;
